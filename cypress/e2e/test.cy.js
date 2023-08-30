9c0d20fd87eadd89e71a6fb321690869c6e8c69e
Olatunde093 <skm.ajide@gmail.com>
2023-08-30 10:47:45 +0100

    Generating random test data

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,5 +1,6 @@
 ///<reference types="cypress"/>
 import test from "../../Page/test.cy"
+import {faker} from '@faker-js/faker';
 
 const tests = new test;
 
@@ -10,15 +11,23 @@ describe("check the sign up button",()=>{
         
     })
     it("The submit button should be enabled",()=>{
+
+    const firstname = faker.person.firstName();
+    const lastname = faker.person.lastName();
+    const email = faker.internet.email();
+    const phone = faker.phone.number();
+    const business = faker.company.name()
+
+
         cy.get("[class='text-sm lg\:text-base text-\[\#02084B80\]'] [class]").click()
-        tests.Firtsname().type("John")
-        tests.Lastname().type("doe")
-        tests.Email().type("doe@example.com")
-        tests.BusinessName().type("Creative Gallery")
-        tests.country().type("Nigeria")
-        tests.selectCountry().click()
-        tests.selectCountry().should('contain','Nigeria')
-        tests.PhoneNumber().type("0812343454")
+        tests.Firtsname().type(firstname)
+        tests.Lastname().type(lastname)
+        tests.Email().type(email)
+        tests.BusinessName().type(business)
+       // tests.country().type("Nigeria")
+        //tests.selectCountry().click()
+      //  tests.selectCountry().should('contain','Nigeria')
+        tests.PhoneNumber().type(phone)
         tests.Password().type("Windows@123")
         tests.ConfirmPassword().type("Windows@123")
         tests.terms().click()
